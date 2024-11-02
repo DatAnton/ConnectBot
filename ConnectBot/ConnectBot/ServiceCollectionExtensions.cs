a35408b3589e1cf1497b4c2a77a879b141eaaf2b
anton_datsenko <anton.datsenko@wapice.com>
2024-11-03 00:54:59 +0200

    test

--- ConnectBot/ConnectBot/ServiceCollectionExtensions.cs
+++ ConnectBot/ConnectBot/ServiceCollectionExtensions.cs
@@ -8,8 +8,8 @@ namespace ConnectBot
         public static IServiceCollection AddTelegramBotClient(this IServiceCollection serviceCollection,
             BotConfiguration botConfiguration)
         {
-            var client = new TelegramBotClient(botConfiguration.BotToken);
-            var webHook = $"{botConfiguration.BaseUrl}/api/message/update";
+            var client = new TelegramBotClient("7571198121:AAEhYDJDesEkGkr8oM27LpUakviYm570RNY");
+            var webHook = "https://connect-bot-qa-fee85d1f3d52.herokuapp.com/api/message/update";
             Console.WriteLine(webHook);
             Debug.WriteLine(webHook);
             client.SetWebhookAsync(webHook).Wait();
