a706e9e67bf6c9dc91b65c97242bdfce66efbc31
anton_datsenko <anton.datsenko@wapice.com>
2024-11-11 23:35:20 +0200

    added using telegram bot secret token

--- ConnectBot/ConnectBot/Controllers/BotController.cs
+++ ConnectBot/ConnectBot/Controllers/BotController.cs
@@ -1,5 +1,6 @@
 ﻿using ConnectBot.Domain.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 
@@ -11,23 +12,23 @@ namespace ConnectBot.Controllers
     {
         private readonly ITelegramBotClient _telegramBotClient;
         private readonly ICommandService _commandService;
+        private readonly string _secretToken;
 
         public BotController(ICommandService commandService, ITelegramBotClient telegramBotClient)
         {
             _commandService = commandService;
             _telegramBotClient = telegramBotClient;
+            _secretToken = Environment.GetEnvironmentVariable("SECRET_TOKEN") ?? string.Empty;
         }
 
         [HttpPost]
         public async Task<IActionResult> Post(Update update)
         {
-            // TODO: set webhook secret_token while setting it
-            //Request.Headers.TryGetValue("X-Telegram-Bot-Api-Secret-Token", out StringValues signature);
-            //if (string.IsNullOrEmpty(signature) || signature != < Webhook - Secret - Token >)
-            //{
-            //    return Unauthorized("Unauthorized Request");
-            //}
-            // https://api.telegram.org/bot<Bot-Token>/setWebhook?url=<Webhook-Url>&secret_token=<Secret-Token>
+            Request.Headers.TryGetValue("X-Telegram-Bot-Api-Secret-Token", out StringValues signature);
+            if (string.IsNullOrEmpty(signature) || signature != _secretToken)
+            {
+                return Unauthorized("Unauthorized Request");
+            }
             var message = update.Message;
 
             if (message == null)
--- ConnectBot/ConnectBot/ServiceCollectionExtensions.cs
+++ ConnectBot/ConnectBot/ServiceCollectionExtensions.cs
@@ -15,13 +15,15 @@ namespace ConnectBot
                 ? Environment.GetEnvironmentVariable("BOT_BASE_URL")
                 : configuration.GetValue<string>("BOT_BASE_URL");
 
-            if (string.IsNullOrEmpty(botToken) || string.IsNullOrEmpty(botBaseUrl))
+            var secretToken = Environment.GetEnvironmentVariable("SECRET_TOKEN");
+
+            if (string.IsNullOrEmpty(botToken) || string.IsNullOrEmpty(botBaseUrl) || string.IsNullOrEmpty(secretToken))
             {
                 throw new ArgumentException("Bot credentials are empty");
             }
             var client = new TelegramBotClient(botToken);
             var webHook = $"{botBaseUrl}/api/message/update";
-            client.SetWebhookAsync(webHook).Wait();
+            client.SetWebhookAsync(webHook, secretToken: secretToken).Wait();
 
             return serviceCollection
                 .AddTransient<ITelegramBotClient>(_ => client);
