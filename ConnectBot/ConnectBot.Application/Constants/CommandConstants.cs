a678ab452f1d1743954163e9cf2c522df619ac24
anton_datsenko <anton.datsenko@wapice.com>
2025-02-15 16:30:54 +0200

    changed text for donate yout team

--- ConnectBot/ConnectBot.Application/Constants/CommandConstants.cs
+++ ConnectBot/ConnectBot.Application/Constants/CommandConstants.cs
@@ -9,7 +9,7 @@
         public const string FeedbackCommand = $"{EmojiConstants.NoteEmoji} Связаться с командой";
         public const string AllParticipationsCommand = $"{EmojiConstants.ToolEmoji} Все учасники";
         public const string AllParticipationsNumbersCommand = $"{EmojiConstants.ToolEmoji}{EmojiConstants.NumbersEmoji} Все учасники(номера)";
-        public const string DonateYouthTeamCommand = $"{EmojiConstants.RedHeartEmoji} Спонсировать молодежную команду";
+        public const string DonateYouthTeamCommand = $"{EmojiConstants.RedHeartEmoji} Пожертвовать на развитие";
         public const string IceBreakerCommand = $"{EmojiConstants.IceEmoji}{EmojiConstants.RedCrossEmoji} Ice Breaker";
     }
 }
