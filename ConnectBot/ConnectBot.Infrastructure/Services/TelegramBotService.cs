077a37eefb301c0280eacb82abe03cc7f6b84942
anton_datsenko <anton.datsenko@wapice.com>
2025-02-20 00:09:39 +0200

    removed logging on start

--- ConnectBot/ConnectBot.Infrastructure/Services/TelegramBotService.cs
+++ ConnectBot/ConnectBot.Infrastructure/Services/TelegramBotService.cs
@@ -50,15 +50,7 @@ namespace ConnectBot.Infrastructure.Services
                 });
                 var menuButton = new MenuButtonDefault();
                 await _botClient.SetChatMenuButtonAsync(user.ChatId, menuButton);
-                await _applicationDbContext.Logs.AddAsync(new Log()
-                {
-                    ChatId = user.ChatId,
-                    MessageText = $"Deleted for {user.ChatId}",
-                    Message = $"{user.Id}",
-                    CreatedAt = DateTime.UtcNow
-                }, CancellationToken.None);
             }
-            await _applicationDbContext.SaveChangesAsync(CancellationToken.None);
 
             //reassign for admins
             var admins = await _userService.GetUserAdmins(CancellationToken.None);
@@ -70,15 +62,7 @@ namespace ConnectBot.Infrastructure.Services
                 });
                 var menuButton = new MenuButtonCommands();
                 await _botClient.SetChatMenuButtonAsync(admin.ChatId, menuButton);
-                await _applicationDbContext.Logs.AddAsync(new Log()
-                {
-                    ChatId = admin.ChatId,
-                    MessageText = $"Admin set for {admin.ChatId}",
-                    Message = $"{admin.Id}",
-                    CreatedAt = DateTime.UtcNow
-                }, CancellationToken.None);
             }
-            await _applicationDbContext.SaveChangesAsync(CancellationToken.None);
         }
 
         public async Task SetClientLoading(ChatId chatId)
